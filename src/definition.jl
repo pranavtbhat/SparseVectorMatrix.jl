b7e494dc9ccef77346a3c1d9378bc198e5a42f5b
Pranav <pranavtbhat@gmail.com>
2016-04-05 01:22:02 +0530

    Added setindex!

--- src/definition.jl
+++ src/definition.jl
@@ -1,4 +1,4 @@
-import Base: size, nnz, show, getindex, full, hcat, vcat, hvcat
+import Base: size, nnz, show, getindex, setindex!, full, hcat, vcat, hvcat
 
 export SVMatrix
 
@@ -27,12 +27,12 @@ function show(io::IO, x::SVMatrix)
     for c in 1 : x.n
         sv = x.svlist[c]
         for r in 1 : nnz(sv)
-            println("\t[$r, $c] = $(sv[r])")
+            write(io, "\t[$(sv.nzind[r]), $c] = $(sv[r])\n")
         end
     end
 end
 
-### Indexing
+### GetIndex
 
 # Translations
 getindex(A::SVMatrix, I::Tuple{Integer,Integer}) = getindex(A, I[1], I[2])
@@ -41,6 +41,7 @@ getindex(A::SVMatrix, r, ::Colon) = getindex(A, r, 1:size(A, 2))
 getindex(A::SVMatrix, ::Colon, c) = getindex(A, 1:size(A, 1), c)
 
 # Unit Indexing
+
 function getindex(A::SVMatrix, r::Integer, c::Integer)
     if !(1 <= r <= A.m && 1 <= c <= A.n)
         throw(BoundsError())
@@ -54,6 +55,44 @@ getindex(A::SVMatrix, rr::Range, c::Integer) = getindex(A.svlist[c], rr)
 function getindex(A::SVMatrix, r::Integer, cr::Range)
     [getindex(A.svlist[c], r) for c in cr]
 end
+
+# Todo : Array Indexing
+
+### SetIndex
+
+# Translations
+setindex!(A::SVMatrix, v, I::Tuple{Integer,Integer}) = setindex!(A, v, I[1], I[2])
+setindex!(A::SVMatrix, x, ::Colon)          = setindex!(A, x, 1:length(A))
+
+setindex!(A::SVMatrix, x, ::Colon, ::Colon) = setindex!(A, x, 1:size(A, 1), 1:size(A,2))
+setindex!(A::SVMatrix, x, ::Colon, j::Integer) = setindex!(A, x, 1:size(A, 1), j)
+setindex!(A::SVMatrix, x, i::Integer, ::Colon) = setindex!(A, x, i, 1:size(A, 2))
+
+# Unit Indexing
+
+function setindex!(A::SVMatrix, v, r::Integer, c::Integer)
+    if !(1 <= r <= A.m && 1 <= c <= A.n)
+        throw(BoundsError())
+    end
+    setindex!(A.svlist[c], v, r)
+end
+
+# Range Indexing
+function setindex!(A::SVMatrix, v, r::Integer, cr::Range)
+    if length(v) != length(cr)
+        throw(DimensionMismatch(""))
+    end
+    count = 1
+    for i in cr
+        setindex!(A.svlist[i], v[count], r)
+        count += 1
+    end
+    v
+end
+
+setindex!(A::SVMatrix, v, rr::Range, c::Integer) = setindex!(A.svlist[c], v, rr)
+
+
 
 ### Conversion
 full(A::SVMatrix) = hcat(map(full, A.svlist)...)
