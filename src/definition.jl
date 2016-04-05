35d03d7f76f8fb13449443cf75675972235da611
Pranav <pranavtbhat@gmail.com>
2016-04-05 15:15:32 +0530

    Rename SVMatrix to SparseMatrixCD

--- src/definition.jl
+++ src/definition.jl
@@ -1,14 +1,14 @@
 import Base: size, nnz, show, getindex, setindex!, full, hcat, vcat, hvcat
 
-export SVMatrix
+export SparseMatrixCD
 
 
-type SVMatrix{Tv, Ti}
+type SparseMatrixCD{Tv, Ti}
     m::Int
     n::Int
     svlist::Vector{SparseVector{Tv, Ti}}
 
-    function SVMatrix(m::Integer, n::Integer, svlist)
+    function SparseMatrixCD(m::Integer, n::Integer, svlist)
         m < 0 && throw(ArgumentError("number of rows (m) must be ≥ 0, got $m"))
         n < 0 && throw(ArgumentError("number of columns (n) must be ≥ 0, got $n"))
         new(Int(m), Int(n), svlist)
@@ -18,14 +18,14 @@ end
 #### Basic Properties
 
 # Size
-size(x::SVMatrix) = (x.m, x.n)
-size(x::SVMatrix, i::Integer) = (1<= i <=2)? size(x)[UInt(i)] : 1
+size(x::SparseMatrixCD) = (x.m, x.n)
+size(x::SparseMatrixCD, i::Integer) = (1<= i <=2)? size(x)[UInt(i)] : 1
 
 # NNZ
-nnz(x::SVMatrix) = mapreduce(nnz, +, 0, x.svlist)
+nnz(x::SparseMatrixCD) = mapreduce(nnz, +, 0, x.svlist)
 
 # Show
-function show{Tv, Ti}(io::IO, x::SVMatrix{Tv, Ti})
+function show{Tv, Ti}(io::IO, x::SparseMatrixCD{Tv, Ti})
     write(io, "SparseVectorMatrix{$Tv, $Ti}($(x.m) X $(x.n)) with $(nnz(x)) entries:\n")
     for c in 1 : x.n
         sv = x.svlist[c]
@@ -36,18 +36,18 @@ function show{Tv, Ti}(io::IO, x::SVMatrix{Tv, Ti})
 end
 
 # Copy
-copy{Tv, Ti}(x::SVMatrix{Tv, Ti}) = SVMatrix{Tv, Ti}(x.m, x.n, deepcopy(x.svlist))
+copy{Tv, Ti}(x::SparseMatrixCD{Tv, Ti}) = SparseMatrixCD{Tv, Ti}(x.m, x.n, deepcopy(x.svlist))
 
 ### GetIndex
 
 # Translations
-getindex(A::SVMatrix, I::Tuple{Integer,Integer}) = getindex(A, I[1], I[2])
-getindex(A::SVMatrix, ::Colon, ::Colon) = copy(A)
-getindex(A::SVMatrix, r, ::Colon) = getindex(A, r, 1 : size(A, 2))
-getindex(A::SVMatrix, ::Colon, c) = getindex(A, 1 : size(A, 1), c)
+getindex(A::SparseMatrixCD, I::Tuple{Integer,Integer}) = getindex(A, I[1], I[2])
+getindex(A::SparseMatrixCD, ::Colon, ::Colon) = copy(A)
+getindex(A::SparseMatrixCD, r, ::Colon) = getindex(A, r, 1 : size(A, 2))
+getindex(A::SparseMatrixCD, ::Colon, c) = getindex(A, 1 : size(A, 1), c)
 
 # Unit Indexing
-function getindex(A::SVMatrix, r::Integer, c::Integer)
+function getindex(A::SparseMatrixCD, r::Integer, c::Integer)
     if !(1 <= r <= A.m && 1 <= c <= A.n)
         throw(BoundsError())
     end
@@ -55,41 +55,41 @@ function getindex(A::SVMatrix, r::Integer, c::Integer)
 end
 
 # Range Indexing
-getindex(A::SVMatrix, rr::Range, c::Integer) = getindex(A.svlist[c], rr)
-getindex(A::SVMatrix, r::Integer, cr::Range) = sparse([getindex(A.svlist[c], r) for c in cr])
+getindex(A::SparseMatrixCD, rr::Range, c::Integer) = getindex(A.svlist[c], rr)
+getindex(A::SparseMatrixCD, r::Integer, cr::Range) = sparse([getindex(A.svlist[c], r) for c in cr])
 
-function getindex{Tv, Ti}(A::SVMatrix{Tv, Ti}, rr::Range, cr::Range)
+function getindex{Tv, Ti}(A::SparseMatrixCD{Tv, Ti}, rr::Range, cr::Range)
     m = length(rr)
     n = length(cr)
     svlist = [getindex(A.svlist[c], rr) for c in cr]
-    SVMatrix{Tv, Ti}(m, n, svlist)
+    SparseMatrixCD{Tv, Ti}(m, n, svlist)
 end
 
 # Array Indexing
-getindex(A::SVMatrix, rv::AbstractVector, c::Integer) = getindex(A.svlist[c], rv)
-getindex(A::SVMatrix, r::Integer, cv::AbstractVector) = sparse([getindex(A.svlist[c], r) for c in cv])
+getindex(A::SparseMatrixCD, rv::AbstractVector, c::Integer) = getindex(A.svlist[c], rv)
+getindex(A::SparseMatrixCD, r::Integer, cv::AbstractVector) = sparse([getindex(A.svlist[c], r) for c in cv])
 
-function getindex{Tv, Ti}(A::SVMatrix{Tv, Ti}, rv::AbstractVector, cv::AbstractVector)
+function getindex{Tv, Ti}(A::SparseMatrixCD{Tv, Ti}, rv::AbstractVector, cv::AbstractVector)
     m = length(rv)
     n = length(cv)
     svlist = [getindex(A.svlist[c], rv) for c in cv]
-    SVMatrix{Tv, Ti}(m, n, svlist)
+    SparseMatrixCD{Tv, Ti}(m, n, svlist)
 end
 
 
 ### SetIndex
 
 # Translations
-setindex!(A::SVMatrix, v, I::Tuple{Integer,Integer}) = setindex!(A, v, I[1], I[2])
-setindex!(A::SVMatrix, x, ::Colon) = setindex!(A, x, 1:length(A))
+setindex!(A::SparseMatrixCD, v, I::Tuple{Integer,Integer}) = setindex!(A, v, I[1], I[2])
+setindex!(A::SparseMatrixCD, x, ::Colon) = setindex!(A, x, 1:length(A))
 
-setindex!(A::SVMatrix, x, ::Colon, ::Colon) = setindex!(A, x, 1:size(A, 1), 1:size(A,2))
-setindex!(A::SVMatrix, x, ::Colon, j::Integer) = setindex!(A, x, 1:size(A, 1), j)
-setindex!(A::SVMatrix, x, i::Integer, ::Colon) = setindex!(A, x, i, 1:size(A, 2))
+setindex!(A::SparseMatrixCD, x, ::Colon, ::Colon) = setindex!(A, x, 1:size(A, 1), 1:size(A,2))
+setindex!(A::SparseMatrixCD, x, ::Colon, j::Integer) = setindex!(A, x, 1:size(A, 1), j)
+setindex!(A::SparseMatrixCD, x, i::Integer, ::Colon) = setindex!(A, x, i, 1:size(A, 2))
 
 # Unit Indexing
 
-function setindex!(A::SVMatrix, v, r::Integer, c::Integer)
+function setindex!(A::SparseMatrixCD, v, r::Integer, c::Integer)
     if !(1 <= r <= A.m && 1 <= c <= A.n)
         throw(BoundsError())
     end
@@ -97,7 +97,7 @@ function setindex!(A::SVMatrix, v, r::Integer, c::Integer)
 end
 
 # Range Indexing
-function setindex!(A::SVMatrix, v, r::Integer, cr::Range)
+function setindex!(A::SparseMatrixCD, v, r::Integer, cr::Range)
     if length(v) != length(cr)
         throw(DimensionMismatch(""))
     end
@@ -109,18 +109,18 @@ function setindex!(A::SVMatrix, v, r::Integer, cr::Range)
     v
 end
 
-setindex!(A::SVMatrix, v, rr::Range, c::Integer) = setindex!(A.svlist[c], v, rr)
+setindex!(A::SparseMatrixCD, v, rr::Range, c::Integer) = setindex!(A.svlist[c], v, rr)
 
 # Todo: Array indexing
 
 ### Conversion
-full(A::SVMatrix) = hcat(map(full, A.svlist)...)
+full(A::SparseMatrixCD) = hcat(map(full, A.svlist)...)
 
 
 ### Concatenation
 
 # Horizontal Concatenation
-function hcat(A::SVMatrix...)
+function hcat(A::SparseMatrixCD...)
     m = A[1].m
     e = A[1]
     for i in 1 : length(A)
@@ -134,11 +134,11 @@ function hcat(A::SVMatrix...)
 
     n = mapreduce(x->x.n, +, 0, A)
     svlist = vcat(map(x->x.svlist, A)...)
-    SVMatrix{Tv, Ti}(m, n, svlist)
+    SparseMatrixCD{Tv, Ti}(m, n, svlist)
 end
 
 # Vertical Concatenation
-function vcat(A::SVMatrix...)
+function vcat(A::SparseMatrixCD...)
     m = mapreduce(x->x.m, +, 0, A)
     e = A[1]
     for i in 1 : length(A)
@@ -155,13 +155,13 @@ function vcat(A::SVMatrix...)
     for c in 1 : n
         svlist[c] = vcat(map(x->x.svlist[c], A)...)
     end
-    SVMatrix{Tv, Ti}(m, n, svlist)
+    SparseMatrixCD{Tv, Ti}(m, n, svlist)
 end
 
 # Grid Concatenation
-function hvcat(rows::Tuple{Vararg{Int}}, A::SVMatrix...)
+function hvcat(rows::Tuple{Vararg{Int}}, A::SparseMatrixCD...)
     nbr = length(rows)
-    tmp_rows = Array(SVMatrix, nbr)
+    tmp_rows = Array(SparseMatrixCD, nbr)
     k = 0
     @inbounds for i = 1 : nbr
         tmp_rows[i] = hcat(A[(1 : rows[i]) + k]...)
--- src/randgen.jl
+++ src/randgen.jl
@@ -1,5 +1,5 @@
 export spmrand
 
 function spmrand(m::Integer, n::Integer, d::Float64)
-    SVMatrix{Float64, Int}(m, n, [sprand(m, d) for c in 1:n])
+    SparseMatrixCD{Float64, Int}(m, n, [sprand(m, d) for c in 1:n])
 end
