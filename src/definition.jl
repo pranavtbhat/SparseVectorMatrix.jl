a878ba0faa2617cb9f0e9d78453ee3974f7a88f6
Pranav <pranavtbhat@gmail.com>
2016-04-05 15:07:37 +0530

    Stronger typing + getindex with vectors

--- src/definition.jl
+++ src/definition.jl
@@ -3,27 +3,30 @@ import Base: size, nnz, show, getindex, setindex!, full, hcat, vcat, hvcat
 export SVMatrix
 
 
-type SVMatrix
+type SVMatrix{Tv, Ti}
     m::Int
     n::Int
-    svlist::Vector{SparseVector}
+    svlist::Vector{SparseVector{Tv, Ti}}
 
-    function SVMatrix(m::Integer, n::Integer, svlist::Vector{SparseVector})
+    function SVMatrix(m::Integer, n::Integer, svlist)
         m < 0 && throw(ArgumentError("number of rows (m) must be ≥ 0, got $m"))
         n < 0 && throw(ArgumentError("number of columns (n) must be ≥ 0, got $n"))
         new(Int(m), Int(n), svlist)
     end
 end
 
-# Basic Properties
+#### Basic Properties
+
+# Size
 size(x::SVMatrix) = (x.m, x.n)
 size(x::SVMatrix, i::Integer) = (1<= i <=2)? size(x)[UInt(i)] : 1
 
+# NNZ
 nnz(x::SVMatrix) = mapreduce(nnz, +, 0, x.svlist)
 
 # Show
-function show(io::IO, x::SVMatrix)
-    write(io, "SparseVectorMatrix($(x.m) X $(x.n)) with $(nnz(x)) entries:\n")
+function show{Tv, Ti}(io::IO, x::SVMatrix{Tv, Ti})
+    write(io, "SparseVectorMatrix{$Tv, $Ti}($(x.m) X $(x.n)) with $(nnz(x)) entries:\n")
     for c in 1 : x.n
         sv = x.svlist[c]
         for r in 1 : nnz(sv)
@@ -32,16 +35,18 @@ function show(io::IO, x::SVMatrix)
     end
 end
 
+# Copy
+copy{Tv, Ti}(x::SVMatrix{Tv, Ti}) = SVMatrix{Tv, Ti}(x.m, x.n, deepcopy(x.svlist))
+
 ### GetIndex
 
 # Translations
 getindex(A::SVMatrix, I::Tuple{Integer,Integer}) = getindex(A, I[1], I[2])
 getindex(A::SVMatrix, ::Colon, ::Colon) = copy(A)
-getindex(A::SVMatrix, r, ::Colon) = getindex(A, r, 1:size(A, 2))
-getindex(A::SVMatrix, ::Colon, c) = getindex(A, 1:size(A, 1), c)
+getindex(A::SVMatrix, r, ::Colon) = getindex(A, r, 1 : size(A, 2))
+getindex(A::SVMatrix, ::Colon, c) = getindex(A, 1 : size(A, 1), c)
 
 # Unit Indexing
-
 function getindex(A::SVMatrix, r::Integer, c::Integer)
     if !(1 <= r <= A.m && 1 <= c <= A.n)
         throw(BoundsError())
@@ -51,18 +56,32 @@ end
 
 # Range Indexing
 getindex(A::SVMatrix, rr::Range, c::Integer) = getindex(A.svlist[c], rr)
+getindex(A::SVMatrix, r::Integer, cr::Range) = sparse([getindex(A.svlist[c], r) for c in cr])
 
-function getindex(A::SVMatrix, r::Integer, cr::Range)
-    [getindex(A.svlist[c], r) for c in cr]
+function getindex{Tv, Ti}(A::SVMatrix{Tv, Ti}, rr::Range, cr::Range)
+    m = length(rr)
+    n = length(cr)
+    svlist = [getindex(A.svlist[c], rr) for c in cr]
+    SVMatrix{Tv, Ti}(m, n, svlist)
 end
 
-# Todo : Array Indexing
+# Array Indexing
+getindex(A::SVMatrix, rv::AbstractVector, c::Integer) = getindex(A.svlist[c], rv)
+getindex(A::SVMatrix, r::Integer, cv::AbstractVector) = sparse([getindex(A.svlist[c], r) for c in cv])
+
+function getindex{Tv, Ti}(A::SVMatrix{Tv, Ti}, rv::AbstractVector, cv::AbstractVector)
+    m = length(rv)
+    n = length(cv)
+    svlist = [getindex(A.svlist[c], rv) for c in cv]
+    SVMatrix{Tv, Ti}(m, n, svlist)
+end
+
 
 ### SetIndex
 
 # Translations
 setindex!(A::SVMatrix, v, I::Tuple{Integer,Integer}) = setindex!(A, v, I[1], I[2])
-setindex!(A::SVMatrix, x, ::Colon)          = setindex!(A, x, 1:length(A))
+setindex!(A::SVMatrix, x, ::Colon) = setindex!(A, x, 1:length(A))
 
 setindex!(A::SVMatrix, x, ::Colon, ::Colon) = setindex!(A, x, 1:size(A, 1), 1:size(A,2))
 setindex!(A::SVMatrix, x, ::Colon, j::Integer) = setindex!(A, x, 1:size(A, 1), j)
@@ -92,7 +111,7 @@ end
 
 setindex!(A::SVMatrix, v, rr::Range, c::Integer) = setindex!(A.svlist[c], v, rr)
 
-
+# Todo: Array indexing
 
 ### Conversion
 full(A::SVMatrix) = hcat(map(full, A.svlist)...)
@@ -109,9 +128,13 @@ function hcat(A::SVMatrix...)
             error("All input matrices must have the same number of rows")
         end
     end
+
+    Tv = promote_type(map(x->eltype(x.svlist[1].nzval), A)...)
+    Ti = promote_type(map(x->eltype(x.svlist[1].nzind), A)...)
+
     n = mapreduce(x->x.n, +, 0, A)
     svlist = vcat(map(x->x.svlist, A)...)
-    SVMatrix(m, n, svlist)
+    SVMatrix{Tv, Ti}(m, n, svlist)
 end
 
 # Vertical Concatenation
@@ -123,9 +146,16 @@ function vcat(A::SVMatrix...)
             error("All input matrices must have the same number of columns")
         end
     end
+
+    Tv = promote_type(map(x->eltype(x.svlist[1].nzval), A)...)
+    Ti = promote_type(map(x->eltype(x.svlist[1].nzind), A)...)
+
     n = A[1].n
-    svlist = SparseVector[vcat(map(x->x.svlist[c], A)...) for c in 1:n]
-    SVMatrix(m, n, svlist)
+    svlist = Vector{SparseVector{Tv, Ti}}(n)
+    for c in 1 : n
+        svlist[c] = vcat(map(x->x.svlist[c], A)...)
+    end
+    SVMatrix{Tv, Ti}(m, n, svlist)
 end
 
 # Grid Concatenation
--- src/randgen.jl
+++ src/randgen.jl
@@ -1,5 +1,5 @@
 export spmrand
 
 function spmrand(m::Integer, n::Integer, d::Float64)
-    SVMatrix(m, n, SparseVector[sprand(m, d) for c in 1:n])
+    SVMatrix{Float64, Int}(m, n, [sprand(m, d) for c in 1:n])
 end
